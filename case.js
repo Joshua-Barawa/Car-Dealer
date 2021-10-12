81897c9aba383c77589cf794d1d25db8f1239d26
erick njeru <erick.njeru@student.moringaschool.com>
2021-10-12 12:50:50 +0300

    color adjusts

---
+++ case.js
@@ -0,0 +1,40 @@
+var crazyCase
+crazyCase = "mY nAmE is caPtAIN AMErica";
+console.log(crazyCase.toLowerCase())
+
+var crazyCase
+crazyCase = "mY nAmE is caPtAIN AMErica";
+console.log(crazyCase.toUpperCase())
+
+var age 
+age = 5
+console.log(age)
+age = age + 2
+console.log(age)
+
+age +=3
+console.log(age)
+
+var nunber 
+number = 15
+console.log(number)
+nunber = 23 - nunber
+
+var number
+number = 20
+console.log(number)
+number += 3;
+console.log(number)
+
+var row 
+row = 45
+console.log(row)
+row -= 11
+console.log(row)
+
+var newNumber
+newNumber = 15 
+console.log(newNumber)
+ newNumber === 10
+ console.log(newNumber === 10)
+
---
+++ strings.js
@@ -0,0 +1,19 @@
+var longestWord
+longestWord = "Freeaavvssdieee"
+console.log(longestWord)
+console.log(longestWord.length)
+
+var wide 
+wide = "flabbergasted"
+console.log(wide)
+console.log(wide.length)
+
+console.log(wide[0])
+console.log(wide[3])
+console.log(longestWord.slice(3, 13));
+console.log(longestWord.slice(4, 14))
+
+
+var crazyCase 
+crazyCase = "tHIS iS kENya";
+console.log(carzyCase.toLowerCase);
