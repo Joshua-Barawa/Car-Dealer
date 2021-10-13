2b5a09e9690c09961b4a2add34ffbd2f30bfead8
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2021-10-13 08:54:32 +0300

    Added js functions

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 
 
-
-
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+  }
